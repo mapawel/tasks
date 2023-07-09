4270ebf9d976c3ada035dcaaecbcf9440a145b09
mapawel <pawelma@gmail.com>
2023-07-10 01:48:29 +0200

    fix: task delete method

--- app.ts
+++ app.ts
@@ -66,7 +66,7 @@ class App {
   await app.appInit();
   console.log(
     await onlyForDevelopmentGetOrCreateTestUser(
-      'testUser@mail.com',
+      'testUser2@mail.com',
       'notHashedPassForDevelopmentOnly'
     )
   );
